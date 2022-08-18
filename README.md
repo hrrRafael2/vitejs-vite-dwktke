eee1b6760c7c9397feea35fb032076ac07baf79a
hrrRafael2 <rafaelhruiz@gmail.com>
2022-08-18 14:31:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-dwktke
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-dwktke)
---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Hi</h1>
+      <h2>HI 2</h2>
+    </>
+  );
+}
+
+export default App;
